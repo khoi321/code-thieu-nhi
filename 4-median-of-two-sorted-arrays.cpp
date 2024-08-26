d6e726c5457885a5916abf041c0a24540050cb93
VoKhoi <97377567+khoi321@users.noreply.github.com>
2024-08-26 16:35:54 +0700

    4-median-of-two-sorted-arrays.cpp

---
+++ 4-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,m;
+    cin>>n>>m;
+    vector<int> v1(n);
+    vector<int> v2(m);
+
+    for(int i=0;i<n;i++){
+        cin>>v1[i];
+    }
+    for(int i=0;i<m;i++){
+        cin>>v2[i];
+    }
+    v1.insert(v1.end(),v2.begin(), v2.end());
+    sort(v1.begin(),v1.end());
+    if(n+m%2==0){
+        cout<<(v1[(n+m)/2]+v1[(n+m)/2-1])/2.0;
+    }
+    else{
+        cout<<v1[(n+m)/2];
+    }
+}
