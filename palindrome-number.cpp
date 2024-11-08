4521f58be30555c79858a497c2e5bfc2f3b322c9
VoKhoi <97377567+khoi321@users.noreply.github.com>
2024-11-08 20:55:37 +0700

    Update palindrome-number.cpp

--- palindrome-number.cpp
+++ palindrome-number.cpp
@@ -1,7 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 int s,nhan=1;
-int main() {
+int main(){
+    iosbase::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n;
     cin>>n;
     int x=n;
